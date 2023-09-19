1bc10602a85f7d08837da4e3a6ad533d3f448a3d
Sajjad <mohammadisajjad2017@gmail.com>
2023-09-19 09:42:30 +0330

    final solution for easy game!

--- pygame/bazi sade/game.py
+++ pygame/bazi sade/game.py
@@ -9,11 +9,13 @@ class Game:
         self.speed = [1, 1]
         self.black = 0, 0, 0
         self.screen = pygame.display.set_mode(self.size)
-        self.ball, self.ballrect = self.load_image("sq.png")
+        self.ballrect ,self.ball= self.load_image("sq.png")
 
     @staticmethod
     def load_image(image_name):
-        pass
+        image = pygame.image.load(image_name)
+        loc = image.get_rect()
+        return loc,image
 
     def move_ball(self):
         ballrect = self.ballrect.move(self.speed)
@@ -24,10 +26,13 @@ class Game:
         self.ballrect = ballrect
 
     def draw(self):
-        pass
+        self.screen.fill(self.black)
+        self.screen.blit(self.ball,self.ballrect)
+        pygame.display.update()
 
     @staticmethod
     def handle_events():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
